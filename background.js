43c310af8a1f10778accae18a73173faeb259f03
WouterMooij <wmooij@gmail.com>
2023-05-02 16:41:33 +0200

    First working prototype. Still work to do:

--- background.js
+++ background.js
@@ -1,8 +1,26 @@
-// background.js
+chrome.commands.onCommand.addListener(function(command) {
+  if (command === "search-on-google") {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var selectedText = "";
+      chrome.tabs.executeScript(tabs[0].id, {code: "window.getSelection().toString();"}, function(selection) {
+        selectedText = selection[0];
+        var newUrl = "https://www.hoptimaal.nl/nl/search/" + encodeURIComponent(selectedText) + "/";
+        chrome.tabs.create({url: newUrl});
+      });
+    });
+  }
+});
 
-let color = '#3aa757';
+chrome.contextMenus.create({
+  title: "Dig it on Hoptimaal",
+  contexts: ["selection"],
+  id: "myContextMenuId"
+});
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "myContextMenuId") {
+    var selectedText = info.selectionText;
+    var newUrl = "https://www.hoptimaal.nl/nl/search/" + encodeURIComponent(selectedText) + "/";
+    chrome.tabs.create({url: newUrl});
+  }
 });
